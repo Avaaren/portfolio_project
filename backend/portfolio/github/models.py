1bfb0ee39d29da6f3e3a4671919c170afcbf1b34
Avaaren <avaaren@mail.ru>
2020-05-26 12:38:44 +0300

    Added tables for github app DB

--- backend/portfolio/github/models.py
+++ backend/portfolio/github/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Repository(models.Model):
+    name = models.CharField('Название репозитория', max_length=50)
+    link = models.CharField('Ссылка на репозиторий', max_length=75)
+    description = models.TextField('Описание репозитория', max_length=2500, default='')
+    number_of_commits = models.PositiveSmallIntegerField('Количество коммитов', default=0)
+
+    def __str__(self):
+        return f'Репозиторий - {self.name}'
