a7301ff6bbdc86e8c4383ce0b9ad0bae23cf2f8b
Avaaren <avaaren@mail.ru>
2020-05-26 12:39:28 +0300

    Added tables for resume app DB

--- backend/portfolio/resume/models.py
+++ backend/portfolio/resume/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class PassedMaterial(models.Model):
+    TYPE_CHOICES = (
+        ('КНИГА', 'Книга'),
+        ('СТАТЬЯ', 'Статья'),
+        ('ВИДЕО', 'Видео'),
+        ('КУРС', 'Курс'),
+    )
+    name = models.CharField('Название материала', max_length=50)
+    link = models.CharField('Сылка на материал', max_length=100)
+    type_of_material = models.CharField('Тип материала',
+                                        max_length=50,
+                                        choices=TYPE_CHOICES,
+                                        default='')
+    mark = models.PositiveSmallIntegerField('Оценка материала', default=0)
+    technology = models.ForeignKey('Technology',
+                                   related_name='material',
+                                   on_delete=models.SET_DEFAULT,
+                                   default=None)
+    is_favorite = models.BooleanField('В избранном', default=False)
+
+    def __str__(self):
+        return '{self.name}'
+
+
+class Technology(models.Model):
+    name = models.CharField('Название технологии', max_length=50)
+    specialization = models.CharField('Направление', max_length=50)
