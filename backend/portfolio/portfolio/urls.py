7a8d73dd4794caf8e8a9cb4e0f70d44cbda3736e
Avaaren <avaaren@mail.ru>
2020-05-28 10:26:37 +0300

    Migrated changes

--- backend/portfolio/portfolio/urls.py
+++ backend/portfolio/portfolio/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('github/', include('github.urls', namespace='github')),
-    path('', include('landing.urls', namespace='landing')),
+    # path('', include('landing.urls', namespace='landing')),
     path('projects/', include('projects.urls', namespace='projects')),
     path('resume/', include('resume.urls', namespace='resume')),
 ]
