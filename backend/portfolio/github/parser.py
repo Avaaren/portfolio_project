1d07d72f722e58411c28bba95739a62dfaa2df28
Avaaren <avaaren@mail.ru>
2020-05-28 13:40:28 +0300

    Added get_pinned_apps logic and little changes for correct work in other methods

--- backend/portfolio/github/parser.py
+++ backend/portfolio/github/parser.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-from .models import Repository
+# from .models import Repository
 
 BASE_URL = 'https://github.com'
 
-def get_urls(url, check_type=None):
+def get_urls(nickname, check_type=None):
 
     if check_type == 'Pinned':
-        html = requests.get(BASE_URL).text
+        html = requests.get(f'{BASE_URL}/{nickname}').text
         soup = BeautifulSoup(html, 'html.parser')
 
         pinned_apps_container = soup.find('div', class_='js-pinned-items-reorder-container')
@@ -17,7 +17,7 @@ def get_urls(url, check_type=None):
         return pinned_apps_links
 
     if check_type == 'All':
-        html = requests.get(BASE_URL.join('/Avaaren?tab=repositories')).text
+        html = requests.get(BASE_URL + '/Avaaren?tab=repositories').text
         soup = BeautifulSoup(html, 'html.parser')
 
         repositories_container = soup.find('div', id='user-repositories-list')
@@ -34,18 +34,28 @@ def get_data(html):
 
     name = soup.find('h1').find('strong').find('a').get_text()
 
-    link = BASE_URL.join(soup.find('h1').find('strong').find('a').get('href'))
+    link = BASE_URL + soup.find('h1').find('strong').find('a').get('href')
 
-    description = soup.find('span', itemprop='about')
+    description = soup.find('span', itemprop='about').get_text().strip()
 
-    number_of_commits = soup.find('li', class_="commits").find('span', class_="num text-emphasized").get_text()
+    number_of_commits = soup.find('li', class_="commits").find('span', class_="num text-emphasized").get_text().strip()
 
+    return (name, link, description, number_of_commits)
 
 def get_pinned_apps():
-    pass
+    nickname = 'Avaaren'
+    check_type = 'Pinned'
+
+    url_list = get_urls(nickname, check_type)
+    for url in url_list:
+        data = get_data(get_html(url))
+        print(data)
+        print('----------------')
 
 def get_all_repos():
     pass
 
 def check_for_updates():
     pass
+
+get_pinned_apps()
