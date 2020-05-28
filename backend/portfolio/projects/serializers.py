a0a1210cc7cf4c64a53e653e955e8087ecc4d397
Avaaren <avaaren@mail.ru>
2020-05-28 11:57:19 +0300

    Added serializer for projects and custom serializer for project images

---
+++ backend/portfolio/projects/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Project, ProjectImage
+
+
+class ProjectImageSerializer(serializers.RelatedField):
+    '''Image serializer for image field in Project'''
+    class Meta:
+        model = ProjectImage
+        exclude = ('project')
+    # Method for representing image as field in ProjectSerializer
+    def to_representation(self, value):
+        return f'{value.image.url}'
+
+class ProjectSerializer(serializers.ModelSerializer):
+    '''Project list'''
+    images = ProjectImageSerializer(source='image', many=True, read_only=True)
+    class Meta:
+        model = Project
+        fields = '__all__'
