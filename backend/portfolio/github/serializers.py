8d2630f8d81d7d77f580fd5dfb197dfefec8824e
Avaaren <avaaren@mail.ru>
2020-05-29 11:30:39 +0300

    Added serializer for pinned apps

--- backend/portfolio/github/serializers.py
+++ backend/portfolio/github/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 
 from .models import Repository
 
-class PinnedApps(object): 
-    def __init__(self, apps): 
-        self.apps = apps 
 
-class PinnedAppsSerizalazer(serializers.Serializer):
-    apps = serializers.ListField(child=serializers.ListField(child=serializers.CharField()))
+class PinnedAppsSerizalazer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Repository
+        fields = '__all__'
