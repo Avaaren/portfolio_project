725e79fdf4c412f9dd604c258401b3a620b26189
Avaaren <avaaren@mail.ru>
2020-05-29 13:35:20 +0300

    Removed celery tasks from view

--- backend/portfolio/github/views.py
+++ backend/portfolio/github/views.py
@@ -3,12 +3,10 @@ from rest_framework.views import APIView
 
 from .models import Repository
 from .serializers import PinnedAppsSerizalazer
-from .tasks import update_repos_table
 
 class PinnedAppView(APIView):
 
     def get(self, request):
-        s = update_repos_table.delay()
         repositories = Repository.objects.all()
         serializer = PinnedAppsSerizalazer(repositories, many=True)
         return Response(serializer.data)
