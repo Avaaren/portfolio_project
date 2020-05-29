5ce538a1f24502efa67c3b8ffdb78ed96f99556c
Avaaren <avaaren@mail.ru>
2020-05-29 14:23:57 +0300

    Added url for material and technology models

---
+++ backend/portfolio/resume/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import PassedMaterial, Technology
+
+class TechnologySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Technology
+        fields = '__all__'
+
+class PassedMaterialSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PassedMaterial
+        fields = '__all__'
+
+
