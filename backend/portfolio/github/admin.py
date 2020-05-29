f9feb77657b5394d67dd4de4747b6a1045215fd5
Avaaren <avaaren@mail.ru>
2020-05-29 09:18:53 +0300

    Registered commits model on admin site

--- backend/portfolio/github/admin.py
+++ backend/portfolio/github/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Repository
+from .models import Repository, Commits
 
 
 @admin.register(Repository)
 class RepositoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'number_of_commits', 'description']
+
+@admin.register(Commits)
+class CommitsAdmin(admin.ModelAdmin):
+    list_display = ['date', 'number_of_commits']
