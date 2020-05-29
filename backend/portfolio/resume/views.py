48b82335f80e34f9113a53e85ee1aa597c935257
Avaaren <avaaren@mail.ru>
2020-05-29 14:24:13 +0300

    Added view to handle get request to resume page

--- backend/portfolio/resume/views.py
+++ backend/portfolio/resume/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from .serializers import PassedMaterialSerializer
+from .models import PassedMaterial
 
-class MainView(View):
-    pass
+class PassedMaterialView(APIView):
+    
+    def get(self, request):
+        materials = PassedMaterial.objects.all()
+        serializer = PassedMaterialSerializer(materials, many=True)
+        return Response(serializer.data)
