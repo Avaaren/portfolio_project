9b6c86a91425c393ebbe058e377f9e91fa1eba53
Avaaren <avaaren@mail.ru>
2020-06-03 18:27:55 +0300

    Changed import of service

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ProjectsList from './ProjectsList'
+import ProjectsList from './components/ProjectsList'
 
 class App extends Component {
 
