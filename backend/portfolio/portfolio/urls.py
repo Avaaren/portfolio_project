4382e2ce5d0fb53fa76cd4e98a4cd92767834b44
Avaaren <avaaren@mail.ru>
2020-06-03 14:45:20 +0300

    added api/ prefix for every url

--- backend/portfolio/portfolio/urls.py
+++ backend/portfolio/portfolio/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('github/', include('github.urls', namespace='github')),
-    # path('', include('landing.urls', namespace='landing')),
-    path('projects/', include('projects.urls', namespace='projects')),
-    path('resume/', include('resume.urls', namespace='resume')),
+    path('api/github/', include('github.urls', namespace='github')),
+    # path('api/', include('landing.urls', namespace='landing')),
+    path('api/projects/', include('projects.urls', namespace='projects')),
+    path('api/resume/', include('resume.urls', namespace='resume')),
 
     path('api_auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
