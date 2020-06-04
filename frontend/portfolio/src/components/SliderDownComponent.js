1e08d117276508b1f6ea075f6efda8173d56b445
Avaaren <avaaren@mail.ru>
2020-06-04 13:44:33 +0300

    Added slider arrow component

---
+++ frontend/portfolio/src/components/SliderDownComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import '../styles/base.css'
+
+export default class SliderDown extends Component {
+    render() {
+        return(
+            <div className="slider-down">
+                <span>
+                    <img src="https://img.icons8.com/carbon-copy/100/000000/chevron-down.png" alt="slider-down" />
+                </span>
+            </div>
+        );
+    }
+
+}
