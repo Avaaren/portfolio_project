af00322717199a919b593413049762c8dde5109c
Avaaren <avaaren@mail.ru>
2020-06-04 15:20:19 +0300

    Added MyContactsSection beta ver. Todo image pathes.

---
+++ frontend/portfolio/src/components/LandingSections/MyContactsSection.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import SliderDownComponent from '../SliderDownComponent'
+import LandingCarousel2 from '../LandingCarousel2'
+
+const image_path = [
+    require("../../img/social-icons/git.svg"),
+    require("../../img/social-icons/hh.png"),
+    require("../../img/social-icons/linkedin.png"),
+    require("../../img/social-icons/tg.png"),
+    require("../../img/social-icons/vk.png"),
+]
+
+export default class MyResumeSection extends Component {
+    render(){
+        return(
+           <div className="container" id="my-contacts-link">
+           <div className="row">
+               <div className="col-12">
+                   <section className="my-contacts">
+                       <h2 className="my-4">Мои контакты</h2>
+                       <h3>Если вы долистали досюда и вы заинтересованы, то связаться можете по следующим контактам</h3>
+   
+   
+                       <LandingCarousel2 image_path={image_path} html_id='contacts-carousel'/>
+                       <SliderDownComponent />
+   
+                   </section>
+               </div>
+           </div>
+       </div>
+        );
+    }
+   }
+   
+
+
+
