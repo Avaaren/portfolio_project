84e7ebb1d99cdc5191a72a58834aa2a0095eb978
Avaaren <avaaren@mail.ru>
2020-06-08 13:35:59 +0300

    Added slider down component behavior

--- frontend/portfolio/src/components/SliderDownComponent.js
+++ frontend/portfolio/src/components/SliderDownComponent.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 import '../styles/base.css'
 
 export default class SliderDown extends Component {
+
+    onClick = (event) => {
+        const node = event.currentTarget.closest('.container')
+        window.scrollTo({
+            top: node.nextSibling.offsetTop,
+            behavior: "smooth"
+       });
+    }
     render() {
         return(
-            <div className="slider-down">
+            <div className="slider-down" onClick={this.onClick}>
                 <span>
                     <img src="https://img.icons8.com/carbon-copy/100/000000/chevron-down.png" alt="slider-down" />
                 </span>
