4022f025dc4d1048fa476709cab96bba0518c8f9
Avaaren <avaaren@mail.ru>
2020-06-08 11:22:03 +0300

    Changed class for className

--- frontend/portfolio/src/components/LandingPageComponents.js
+++ frontend/portfolio/src/components/LandingPageComponents.js
@@ -31,42 +31,42 @@ export default class LandingPage extends Component {
 class Navbar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 {/* navbar logo */}
-                <a href="#" class="navbar-brand">
+                <a href="#" className="navbar-brand">
                     <img src="img/shiba-inu.jpg" alt="logo" width="50" height="50" />
                 </a>
                 {/* Collapsed navbar button */}
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#collapsedNavbar" aria-expanded="false" aria-label="ToggleNavigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#collapsedNavbar" aria-expanded="false" aria-label="ToggleNavigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
                 {/* Collapsing navbar */}
-                <div class="collapse navbar-collapse" id="collapsedNavbar">
-                    <div class="navbar-nav d-flex justify-content-around" id="flexed-navbar-container">
+                <div className="collapse navbar-collapse" id="collapsedNavbar">
+                    <div className="navbar-nav d-flex justify-content-around" id="flexed-navbar-container">
 
-                        <div class="nav-item">
-                            <a href="#about-project-link" class="nav-link">О проекте</a>
+                        <div className="nav-item">
+                            <a href="#about-project-link" className="nav-link">О проекте</a>
                         </div>
 
-                        <div class="nav-item">
-                            <a href="#about-me-link" class="nav-link">Обо мне</a>
+                        <div className="nav-item">
+                            <a href="#about-me-link" className="nav-link">Обо мне</a>
                         </div>
 
-                        <div class="nav-item">
-                            <a href="#my-projects-link" class="nav-link">Мои проекты</a>
+                        <div className="nav-item">
+                            <a href="#my-projects-link" className="nav-link">Мои проекты</a>
                         </div>
 
-                        <div class="nav-item">
-                            <a href="#my-resume-link" class="nav-link">Резюме</a>
+                        <div className="nav-item">
+                            <a href="#my-resume-link" className="nav-link">Резюме</a>
                         </div>
 
-                        <div class="nav-item">
-                            <a href="#my-git-link" class="nav-link">Гит</a>
+                        <div className="nav-item">
+                            <a href="#my-git-link" className="nav-link">Гит</a>
                         </div>
 
-                        <div class="nav-item">
-                            <a href="#my-contacts-link" class="nav-link">Контакты</a>
+                        <div className="nav-item">
+                            <a href="#my-contacts-link" className="nav-link">Контакты</a>
                         </div>
 
                     </div>
