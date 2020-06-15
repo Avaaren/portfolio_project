00cba8e806eefdf24c03f054e5f16478b2a8a547
Avaaren <avaaren@mail.ru>
2020-06-15 11:06:43 +0300

    Progress converting jquery to react

--- frontend/assets/sctipts/landing.js
+++ frontend/assets/sctipts/landing.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
+//Done
     $('.nav-item a').on('click', function(event){
         event.preventDefault();
         let id = $(this).attr('href');
         let destination = $(id).offset().top;
         $('body,html').animate({scrollTop: destination}, 500);
     });
-
+// Done
     $('.slider-down').on('click',function(){
        let sibling = $(this).closest('.container').next('.container');
        let destination = $(sibling).offset().top;
        $('body,html').animate({scrollTop: destination},500);
     });
-
+// Done
     $(window).on('scroll', function(){
         if (window.scrollY > 300){
             $('.to-start-button').show(200);
@@ -20,7 +21,7 @@ $(document).ready(function(){
             $('.to-start-button').hide(200);
         }
     });
-
+// Done
     $('.to-start-button').on('click', function(){
         let nav = $('body').find('nav');
         let destination = $(nav).offset().top;
