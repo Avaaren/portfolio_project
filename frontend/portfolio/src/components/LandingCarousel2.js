30e77288dffb65758a50b10d745857c07ce74055
Avaaren <avaaren@mail.ru>
2020-06-18 18:13:07 +0300

    Changed landing carousels. Now it is splitted for 2. Need to change

--- frontend/portfolio/src/components/LandingCarousel2.js
+++ frontend/portfolio/src/components/LandingCarousel2.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
-const BASE_SERVER_URL = 'http://127.0.0.1:8000'
 
 export default class Carousel extends Component {
     render (){
-        const {projects, html_id} = this.props;
+        const {image_path, html_id, description} = this.props;
         return (
             <div className="carousel-wrapper">
                 <div className="carousel slide" id={html_id} data-ride="carousel">
-                <CarouselIndicators dataTarget={html_id} projectsLength={projects.length}/>
-                <CarouselInner projects={projects} html_id={html_id}/>
+                <CarouselIndicators dataTarget={html_id} length={image_path.length}/>
+                <CarouselInner image_path={image_path} html_id={html_id} description={description}/>
             </div>
             </div>
 
@@ -21,7 +20,7 @@ class CarouselIndicators extends Component {
     render() {
         const listItems = [];
 
-        for (let i = 0; i<this.props.projectsLength; i++) {
+        for (let i = 0; i<this.props.length; i++) {
             let el = <li key={i} data-target={'#'+this.props.dataTarget} data-slide-to={i} className=''></li>;
             listItems.push(el);
         }
@@ -35,9 +34,9 @@ class CarouselIndicators extends Component {
 
 class CarouselInner extends Component {
     render (){
-        const {projects, html_id} = this.props;
-        const carouselItemsList = projects.map( (project, index) => 
-            <CarouselItem key={index} id={index} projectImage={project.images[0]} projectName={project.name} />
+        const {image_path, html_id, description} = this.props;
+        const carouselItemsList = image_path.map( (image, index) => 
+            <CarouselItem key={index} id={index} projectImage={image} projectName={description} />
         );
         return(
             <div className="carousel-inner">
@@ -51,12 +50,12 @@ class CarouselInner extends Component {
 
 class CarouselItem extends Component {
     render () {
-        const { projectName, projectImage, id } = this.props;
+        const { projectImage, id, description } = this.props;
 
         return (
             <div className={`carousel-item ${id===1 ? "active" : ""}`}>
                 <CarouselLink image={projectImage}/>
-                <CarouselCaption description={projectName}/>
+                <CarouselCaption description={description}/>
             </div>
         );
     }
@@ -65,10 +64,9 @@ class CarouselItem extends Component {
 class CarouselLink extends Component {
     render() {
         const {image} = this.props;
-
         return (
             <a href='#' className="carousel-link">
-                <img src={BASE_SERVER_URL+image.image} alt="project-screenshot" className="d-block w-100 carousel-image"/>
+                <img src={image} alt="project-screenshot" className="d-block w-100 carousel-image"/>
             </a>
         );
     }
